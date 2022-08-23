54520adffb35df33230d772c86611bba5a692f18
Alejandro Tellez <alejo082495@gmail.com>
2022-08-22 22:03:08 -0500

    Se añade las imagenes por medio de JS por insercciones

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
-console.log('Esto es una prueba ????????');
+document.addEventListener('DOMContentLoaded', function() {
+    iniciarApp();
+});
+
+function iniciarApp() {
+    crearGaleria();
+}
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes');
+    for (let i = 1; i <= 12; i++) {
+        const imagen = document.createElement('DIV');
+        imagen.innerHTML = `            
+            <img  src="build/img/thumb/${i}.jpg" alt="Imagen ">
+        `;
+        galeria.appendChild(imagen);
+    }
+}
